9f916451d5110d88a71a37b30a0d50ad2870de6f
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-08-24 14:02:53 +0000

    enhance XMDL Modeling perspective; Added Navigator & Package Explorer to the left corner.

--- xmdl.ui/src/chrome/xmdl/ui/perspective/XmdlModelingPerspective.java
+++ xmdl.ui/src/chrome/xmdl/ui/perspective/XmdlModelingPerspective.java
@@ -1,5 +1,6 @@
 package chrome.xmdl.ui.perspective;
 
+import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
@@ -23,14 +24,22 @@ public class XmdlModelingPerspective implements IPerspectiveFactory {
 		layout.setEditorAreaVisible(true);
 		layout.addPerspectiveShortcut(ID_PERSPECTIVE);
 
-		IFolderLayout right = layout.createFolder("right",
-				IPageLayout.RIGHT, (float) 0.66, layout.getEditorArea());
+		String editorArea = layout.getEditorArea();
+
+		IFolderLayout topLeft = layout.createFolder("topLeft",
+				IPageLayout.LEFT, 0.25f, editorArea);
+		topLeft.addView(IPageLayout.ID_RES_NAV);
+		topLeft.addPlaceholder(JavaUI.ID_PACKAGES);
+
+		IFolderLayout right = layout.createFolder("right", IPageLayout.RIGHT,
+				(float) 0.66, editorArea);
 		right.addView(IPageLayout.ID_OUTLINE);
 
 		IFolderLayout bottonRight = layout.createFolder("bottonRight",
 				IPageLayout.BOTTOM, (float) 0.60, "right");
 		bottonRight.addView(IPageLayout.ID_PROP_SHEET);
+		
+		layout.setEditorAreaVisible(true);
 	}
-	
-	
+
 }
