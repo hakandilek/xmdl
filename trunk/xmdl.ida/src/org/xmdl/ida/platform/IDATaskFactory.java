30b47a3818faf3d107ab0774ad2ce2de35cedef1
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-11-05 14:57:14 +0000

    Initial import.

---
+++ trunk/xmdl.ida/src/org/xmdl/ida/platform/IDATaskFactory.java
@@ -0,0 +1,87 @@
+package org.xmdl.ida.platform;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EObject;
+
+import chrome.xmdl.XProject;
+import chrome.xmdl.xgen.AbstractTaskFactory;
+import chrome.xmdl.xgen.Task;
+import chrome.xmdl.xgen.TaskFactory;
+import chrome.xmdl.xgen.Template;
+
+/**
+ * The Task Factory class for the IDA platform. All Code Generation tasks are
+ * defined in this class
+ * 
+ * @author Hakan Dilek
+ * 
+ */
+public class IDATaskFactory extends AbstractTaskFactory implements TaskFactory {
+
+    protected List<Template> templates;
+
+    private List<Task> successorTasks;
+
+    private List<Task> predecessorTasks;
+
+    private static final Logger LOGGER = Logger
+            .getLogger(IDATaskFactory.class);
+
+    public IDATaskFactory() {
+        super();
+        LOGGER.debug(" new instance");
+        init();
+    }
+
+    @Override
+    protected List<Template> getTemplates() {
+        return templates;
+    }
+
+    @Override
+    public List<Task> createPredecessorTasks(XProject project,
+            List<EObject> list) {
+        if (predecessorTasks == null){
+            predecessorTasks = new ArrayList<Task>();
+        }
+        return predecessorTasks;
+    }
+
+    @Override
+    public List<Task> createSuccessorTasks(XProject project, List<EObject> list) {
+        if (successorTasks == null){
+            successorTasks = new ArrayList<Task>();
+        }
+        return successorTasks;
+    }
+
+    private void init() {
+        try {
+            if (templates == null) {
+                templates = new ArrayList<Template>();
+                templates.clear();
+                //build
+
+                // model
+
+                // dao
+
+                // CONTEXT
+
+                // bo
+
+                // ui.web
+
+                //jsp
+
+                //tests
+            }
+        } catch (Throwable e) {
+            LOGGER.warn("FATAL ", e);
+        }
+    }
+
+}
