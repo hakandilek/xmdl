c3a76f712733e3f98c6bbd257f5ed81640d62e1f
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-11-06 22:07:59 +0000

    Adding DAO and Service templates

---
+++ trunk/xmdl.ida/src/org/xmdl/ida/templates/dao/EntityDAO.java
@@ -0,0 +1,31 @@
+package org.xmdl.ida.templates.dao;
+
+import chrome.xmdl.XClass;
+import chrome.xmdl.XPackage;
+import chrome.xmdl.gen.util.XMDLClassHelper;
+import chrome.xmdl.xgen.Template;
+
+/**
+ * The DAO interface template file
+ * 
+ * @author Hakan Dilek
+ *
+ */
+public class EntityDAO extends EntityDAOImpl implements Template {
+
+    public String targetFile(Object object) {
+        XClass cls = (XClass) object;
+        XPackage pkg = cls.getXPackage();
+        StringBuffer buffer = new StringBuffer("core/src/main/java/");
+        buffer.append(XMDLClassHelper.INSTANCE.getQualifiedPath(pkg));
+        buffer.append("/dao/");
+        buffer.append(cls.getName());
+        buffer.append(".java");
+        return buffer.toString();
+    }
+
+    public boolean accept(Object object) {
+        return (object instanceof XClass);
+    }
+
+}
