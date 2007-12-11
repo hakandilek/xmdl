882c8ccab1e0ea2b7bfdc25522271371a650b4ca
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-12-11 15:19:57 +0000

    base ecore (EMF) init code

--- trunk/xmdl/src/chrome/xmdl/plugin/XMDLModelPlugin.java
+++ trunk/xmdl/src/chrome/xmdl/plugin/XMDLModelPlugin.java
@@ -1,9 +1,15 @@
 package chrome.xmdl.plugin;
 
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.osgi.framework.BundleContext;
 
+import chrome.xmdl.XmdlPackage;
 import chrome.xmdl.XmdlTypes;
+import chrome.xmdlbo.XmdlboPackage;
+import chrome.xmdldb.XmdldbPackage;
+import chrome.xmdlgen.XmdlgenPackage;
 
 /**
  * Bundle activator (plugin definition) for XMDL Model plugin. Make Initializations in this class.
@@ -17,6 +23,21 @@ public class XMDLModelPlugin extends Plugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         XmdlTypes.getInstance();
+        
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmdl",
+                new XMIResourceFactoryImpl());
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmdldb",
+                new XMIResourceFactoryImpl());
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmdlbo",
+                new XMIResourceFactoryImpl());
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmdlgen",
+                new XMIResourceFactoryImpl());
+
+        XmdlPackage.eINSTANCE.eClass();
+        XmdldbPackage.eINSTANCE.eClass();
+        XmdlboPackage.eINSTANCE.eClass();
+        XmdlgenPackage.eINSTANCE.eClass();
+
     }
 
     @Override
