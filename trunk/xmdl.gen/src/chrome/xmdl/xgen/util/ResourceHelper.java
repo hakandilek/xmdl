2102ddab2d8784ceb7757ff5ba263fdb8e5998cb
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-12-11 15:21:53 +0000

    Fix ecore resource loading sequence

--- trunk/xmdl.gen/src/chrome/xmdl/xgen/util/ResourceHelper.java
+++ trunk/xmdl.gen/src/chrome/xmdl/xgen/util/ResourceHelper.java
@@ -3,48 +3,25 @@ package chrome.xmdl.xgen.util;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.URIHandlerImpl;
 
 import chrome.xmdl.XProject;
-import chrome.xmdl.XmdlPackage;
-import chrome.xmdlbo.XmdlboPackage;
 import chrome.xmdldb.DModel;
-import chrome.xmdldb.XmdldbPackage;
-import chrome.xmdlgen.XmdlgenPackage;
 
 public class ResourceHelper {
 	private static final Logger LOGGER = Logger.getLogger(ResourceHelper.class);
-
-	private static boolean initialized = false;
-
-	@SuppressWarnings("unchecked")
-	protected static void initialize() {
-		if (!initialized) {
-			Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
-					"*", new XMIResourceFactoryImpl());
-			
-			XmdlPackage.eINSTANCE.eClass();
-			XmdldbPackage.eINSTANCE.eClass();
-			XmdlboPackage.eINSTANCE.eClass();
-			XmdlgenPackage.eINSTANCE.eClass();
-			
-	        XmdlPackage.eINSTANCE.getEFactoryInstance();
-            XmdldbPackage.eINSTANCE.getEFactoryInstance();
-            XmdlboPackage.eINSTANCE.getEFactoryInstance();
-            XmdlgenPackage.eINSTANCE.getEFactoryInstance();
-
-			initialized = true;
-		}
-	}
 
 	public ResourceHelper() {
 		super();
@@ -58,10 +35,8 @@ public class ResourceHelper {
 	}
 
 	public static EObject loadResource(URI uri){
-		initialize();
 		ResourceSet resourceSet = new ResourceSetImpl();
-		URIConverter converter = resourceSet.getURIConverter();
-		uri = converter.normalize(uri);
+        resourceSet.getURIConverter().getURIMap().putAll(EcorePlugin.computePlatformURIMap()); 
 		Resource resource = resourceSet.getResource(uri, true);
 		EObject object = (EObject) resource.getContents().get(0);
 		return object;		
@@ -88,16 +63,27 @@ public class ResourceHelper {
 		resource.getContents().add(root);
 		resourceSet.getResources().add(resource);
 
-		Map<String, Object> saveOptions = new HashMap<String, Object>();
-		saveOptions.put(XMLResource.OPTION_PROCESS_DANGLING_HREF, "");
-		saveOptions.put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
-
-		resource.save(saveOptions);
-		
+		Map<String, Object> options = getSaveOptions();
+		resource.save(options);
 		return resource;		
 	}
 
-	public static Resource saveResource(String path, EObject root) throws IOException {
+	public static Map<String, Object> getSaveOptions() {
+        Map<String, Object> options = new HashMap<String, Object>();
+        options.put(XMLResource.OPTION_PROCESS_DANGLING_HREF, "");
+        options.put(XMLResource.OPTION_EXTENDED_META_DATA, Boolean.TRUE);
+        options.put(Resource.OPTION_SAVE_ONLY_IF_CHANGED,
+                Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
+        /*
+         * this part is added to save type references in the form of
+         * href="platform:/plugin/xmdl/model/types.xmdl#java.lang.String"
+         */
+        options.put(XMLResource.OPTION_URI_HANDLER,
+                new URIHandlerImpl.PlatformSchemeAware());
+        return options;
+    }
+
+    public static Resource saveResource(String path, EObject root) throws IOException {
 		LOGGER.debug("Saving resource @ " + path);
 		URI uri = URI.createURI(path);
 		Resource resource = saveResource(uri, root);
@@ -115,5 +101,22 @@ public class ResourceHelper {
 		URI uri = URI.createURI(path);
 		return loadDModel(uri);		
 	}
+
+    public static ResourceSet saveResources(Map<URI, EObject> resourceMap)
+            throws IOException {
+        ResourceSet resourceSet = new ResourceSetImpl();
+        Map<String, Object> options = getSaveOptions();
+        EList<Resource> resources = resourceSet.getResources();
+        Set<URI> uris = resourceMap.keySet();
+        for (URI uri : uris) {
+            EObject root = resourceMap.get(uri);
+            Resource resource = resourceSet.createResource(uri);
+            resource.getContents().add(root);
+            resources.add(resource);
+            resource.save(options);
+        }
+
+        return resourceSet;
+    }
 
 }
