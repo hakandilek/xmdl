f43fa58947db01ec42797569dd5e8294a98368ad
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2007-12-12 15:58:37 +0000

    add <xd:columnLength/> tag

---
+++ trunk/xmdl.genext.test/src/org/xmdl/genext/taglib/xd/ColumnLengthTagTest.java
@@ -0,0 +1,40 @@
+package org.xmdl.genext.taglib.xd;
+
+import org.osgi.framework.BundleException;
+import org.xmdl.genext.BaseTemplateTestCase;
+
+import chrome.xmdl.XAttribute;
+import chrome.xmdl.XProject;
+import chrome.xmdl.gen.util.Locator;
+import chrome.xmdl.meta.MetaModelHolder;
+import chrome.xmdl.xgen.util.ResourceHelper;
+
+public class ColumnLengthTagTest extends BaseTemplateTestCase {
+
+    private XAttribute attribute;
+    private XProject project;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        project = ResourceHelper.loadProject("model/test.xmdl");
+        attribute = Locator.locateAttribute(project,
+                "xmdl.genext.test/org.xmdl.genext.test/Class1/attribute1");
+        MetaModelHolder.initialize(project);
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        attribute = null;
+    }
+
+    public void testAttributeTypeTag() throws BundleException {
+
+        final String templateClass = ColumnLengthTagTestTemplate.class.getName();
+        setVariable("attribute", attribute);
+        String output = runTemplate(templateClass);
+
+        assertNotNull(output);
+        assertEquals(":15;", output.trim());
+    }
+
+}
---
+++ trunk/xmdl.genext/src/org/xmdl/genext/taglib/xd/ColumnLengthTag.java
@@ -0,0 +1,44 @@
+package org.xmdl.genext.taglib.xd;
+
+import org.eclipse.jet.JET2Context;
+import org.eclipse.jet.JET2Writer;
+import org.eclipse.jet.taglib.JET2TagException;
+import org.eclipse.jet.taglib.TagInfo;
+import org.xmdl.genext.taglib.BaseEmptyTag;
+
+import chrome.xmdl.XAttribute;
+import chrome.xmdl.meta.MetaModelFinder;
+import chrome.xmdldb.DField;
+
+/**
+ * Output the length of the column/field from the DB model corresponding to the
+ * given attribute
+ * 
+ * @author Hakan Dilek
+ * 
+ */
+public class ColumnLengthTag extends BaseEmptyTag {
+
+    public ColumnLengthTag() {
+    }
+
+    @Override
+    public void doAction(TagInfo td, JET2Context context, JET2Writer out)
+            throws JET2TagException {
+        Object object = fetchObject(context, "attribute");
+
+        int len = 0;
+        String result = "";
+        if (object instanceof XAttribute) {
+            XAttribute attribute = (XAttribute) object;
+            DField col = MetaModelFinder.findColumn(attribute);
+            if (col != null) {
+                len = col.getLength();
+                result = "" + len;
+            }
+        }
+        out.write(result);
+
+    }
+
+}
