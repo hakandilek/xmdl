d23c629a6c8a3d29c24d5f851d77ae07a20a1010
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2008-01-17 15:23:08 +0000

    Issue 46: rename chrome.xmdl -> org.xmdl ; Issue 49:Change plugin names xmdl.* -> org.xmdl.*

--- xmdl.ui/src/org/xmdl/xmdl/presentation/XmdlEditor.java
+++ xmdl.ui/src/org/xmdl/xmdl/presentation/XmdlEditor.java
@@ -1465,7 +1465,7 @@ public class XmdlEditor extends MultiPageEditorPart implements
 				Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
 		/*
 		 * this part is added to save type references in the form of
-		 * href="platform:/plugin/xmdl/model/types.xmdl#java.lang.String"
+		 * href="platform:/plugin/org.xmdl.model/model/types.xmdl#java.lang.String"
 		 */
 		saveOptions.put(XMLResource.OPTION_URI_HANDLER,
 				new URIHandlerImpl.PlatformSchemeAware());
