c3308c1058764d69fd7b5c71a4d7e6cb33e5f49d
hakandilek <hakandilek@4326f340-9937-0410-85f0-7193a15cbf30>
2009-07-26 22:06:39 +0000

    add new templates and prepare for code generation

---
+++ branches/release-0.7/org.xmdl.wdl.generator/src/org/xmdl/wdl/gen/utils/ExtensionUtils.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.xmdl.wdl.gen.utils;
+
+import org.xmdl.wdl.Type;
+
+/**
+ * Utility methods used by extensions
+ * 
+ * @author hakan
+ * 
+ */
+public class ExtensionUtils {
+
+	public static boolean isReference(Type t) {
+		// TODO:implement!
+		return false;
+	}
+
+	public static boolean isEnumeration(Type t) {
+		// TODO:implement!
+		return false;
+	}
+
+	public static boolean isEmbed(Type t) {
+		// TODO:implement!
+		return false;
+	}
+
+	public static boolean isPrimitive(Type t) {
+		// TODO:implement!
+		return false;
+	}
+
+	public static boolean isComparable(Type t) {
+		// TODO:implement!
+		return false;
+	}
+
+	public static String simpleName(Type t) {
+		// TODO:implement!
+		return null;
+	}
+
+}
